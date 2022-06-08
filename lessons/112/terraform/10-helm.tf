7e27b480a8f3ec2c513608f60251dc5892d7b37f
Anton Putra <me@antonputra.com>
2022-06-08 20:13:48 +0300

    asd

--- lessons/112/terraform/10-helm.tf
+++ lessons/112/terraform/10-helm.tf
@@ -18,10 +18,25 @@ resource "helm_release" "aws-load-balancer-controller" {
   namespace  = "kube-system"
   version    = "1.4.1"
 
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.demo.id
+  }
 
-  values = [
-    "${file("values.yaml")}"
-  ]
+  set {
+    name  = "image.tag"
+    value = "v2.4.2"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.aws_load_balancer_controller.arn
+  }
 
   depends_on = [
     aws_eks_node_group.private-nodes,
